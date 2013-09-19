531afd26e4c5bbee2fdb1ca15334e9dbaacf53b8
Christopher Bennage <christopher@bennage.com>
2013-09-19 10:52:42 -0700

    route to render a detail page

--- app.js
+++ app.js
@@ -1,6 +1,42 @@
 $(function() {
 	var root = $('#root');
 
+	window.addEventListener('hashchange', handleRoute)
+
+	function handleRoute() {
+		var route = (window.location.hash) 
+			? window.location.hash.replace('#/', '') 
+			: '';
+		
+		if (!isNaN(route)) renderDetail(route);
+	}
+
+	function renderDetail(id) {
+		root.html('');
+
+		$.ajax({
+			url: '/api/' + id + '.json',
+			type: 'GET',
+			dataType: 'json',
+			success: function(data) {
+
+				var dl = $('<dl></dl>')
+					.appendTo(root);
+
+				Object.keys(data).forEach(function(property) {
+					$('<dt>' + property + '</dt>')
+						.appendTo(dl);
+
+					$('<dd>' + data[property] + '</dd>')
+						.appendTo(dl);
+				});
+			},
+			error: function() {
+				alert('I broke :-(')
+			}
+		});
+	}
+
 	$.ajax({
 		url: '/api/list.json',
 		type: 'GET',
