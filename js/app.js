49d88d97c391fc2c2d85cfcd49b61fa0d3cf986b
Christopher Bennage <christopher@bennage.com>
2013-09-21 13:11:42 -0700

    added a generic router in preparation of the edit page

--- js/app.js
+++ js/app.js
@@ -1,24 +1,40 @@
 define(['detailView', 'listView'], function(detailView, listView) {
 
+	function editDetail() {
+		alert('!!!');
+	}
+
 	$(function() {
 
 		var root = $('#root');
 
 		window.addEventListener('hashchange', handleRoute);
 
-		handleRoute();
+		var routes = [
+			[/edit\/(\d+)/, editDetail],
+			[/(\d+)/, detailView],
+			[/.*/, listView]
+		];
 
 		function handleRoute() {
 			var route = (window.location.hash) ? window.location.hash.replace('#/', '') : '';
 
 			root.html('');
 
-			if (route.match(/\d+/) === null) {
-				listView(root);
-			} else {
-				detailView(root, route)
-			};
+			for (var i = 0; i < routes.length; i++) {
+				var r = routes[i];
+				var regex = r[0];
+				var handler = r[1];
+				var m = route.match(regex);
+				if (m !== null) {
+					handler.apply(null, [root].concat(m.slice(1)));
+					return;
+				}
+			}
+
 		}
+
+		handleRoute();
 
 	});
 
