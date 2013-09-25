1fac0cf8a41411c14e095d5cfab4a2a0e7711151
Christopher Bennage <christopher@bennage.com>
2013-09-25 16:59:34 -0700

    00 - initial state

--- public/js/app.js
+++ public/js/app.js
@@ -1,36 +1,3 @@
-define(['detailView', 'listView', 'editView'], function(detailView, listView, editView) {
-
-	$(function() {
-
-		var root = $('#root');
-
-		window.addEventListener('hashchange', handleRoute);
-
-		var routes = [
-			[/edit\/(\d+)/, editView],
-			[/(\d+)/, detailView],
-			[/.*/, listView]
-		];
-
-		function handleRoute() {
-			var route = (window.location.hash) ? window.location.hash.replace('#/', '') : '';
-
-			root.html('');
-
-			for (var i = 0; i < routes.length; i++) {
-				var r = routes[i];
-				var regex = r[0];
-				var handler = r[1];
-				var m = route.match(regex);
-				if (m !== null) {
-					handler.apply(null, [root].concat(m.slice(1)));
-					return;
-				}
-			}
-		}
-
-		handleRoute();
-
-	});
-
+$(function() {
+	alert('hello');
 });
