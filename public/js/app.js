03d6376e7aefcb370921e36b7fa326d691acf3a0
Christopher Bennage <christopher@bennage.com>
2013-09-25 17:43:00 -0700

    03 - adding hashchange detection and a template for the detail view

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,55 @@
 $(function() {
 	var root = $('#root');
 
-	var templateSource = $('#template-item-list').text();
-	var template = Handlebars.compile(templateSource);
+	var templateDetail = Handlebars.compile($('#template-item-detail').text());
+	var templateList = Handlebars.compile($('#template-item-list').text());
 
-	$.ajax({
-		url: '/api/list',
-		type: 'GET',
-		dataType: 'json',
-		success: function(data) {
-			var html = template(data);
-			root.html(html);
-		},
-		error: function() {
-			alert('something went wrong');
+	window.addEventListener('hashchange', handleRoute);
+
+	handleRoute();
+
+	function handleRoute() {
+		var route = (window.location.hash) ? window.location.hash.replace('#/', '') : '';
+
+		root.html('<div>loading</div>');
+
+		if (route.match(/\d+/) === null) {
+			renderList();
+		} else {
+			renderDetail(route);
 		}
-	});
+	}
+
+	function renderDetail(id) {
+
+		$.ajax({
+			url: '/api/' + id,
+			type: 'GET',
+			dataType: 'json',
+			success: function(data) {
+				var html = templateDetail(data);
+				root.html(html);
+			},
+			error: function() {
+				alert('something went wrong');
+			}
+		});
+	}
+
+	function renderList() {
+
+		$.ajax({
+			url: '/api/list',
+			type: 'GET',
+			dataType: 'json',
+			success: function(data) {
+				var html = templateList(data);
+				root.html(html);
+			},
+			error: function() {
+				alert('something went wrong');
+			}
+		});
+	}
 
 });
